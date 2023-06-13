083341d22547471d4370f4c17286ad7a5c4a64d9
Mohamed Gamal <45459739+MhmdGamal@users.noreply.github.com>
2023-06-13 13:38:11 +0300

    done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An End To End Data Science Projects Including All Phases',
+    author='Mohamed Gamal',
+    license='',
+)
